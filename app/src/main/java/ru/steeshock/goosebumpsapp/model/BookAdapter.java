19ec548a3638a272a0bc4dfbcb950d45c3f91136
Eugeny <steeshock@gmail.com>
2019-02-04 21:56:53 +0300

    Save Favorite Books in SharedPref (the last checked book).

--- app/src/main/java/ru/steeshock/goosebumpsapp/model/BookAdapter.java
+++ app/src/main/java/ru/steeshock/goosebumpsapp/model/BookAdapter.java
@@ -2,24 +2,32 @@ package ru.steeshock.goosebumpsapp.model;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import ru.steeshock.goosebumpsapp.R;
 import ru.steeshock.goosebumpsapp.ui.BooksFragment;
 import ru.steeshock.goosebumpsapp.ui.FavoriteBooksFragment;
+import ru.steeshock.goosebumpsapp.ui.MainActivity;
+import ru.steeshock.goosebumpsapp.utils.UserSettings;
 
 import static ru.steeshock.goosebumpsapp.model.BooksInfo.booksDescription;
 import static ru.steeshock.goosebumpsapp.model.BooksInfo.booksNames;
 import static ru.steeshock.goosebumpsapp.model.BooksInfo.imagesCollection;
+import static ru.steeshock.goosebumpsapp.utils.UserSettings.FAVORITE_BOOKS_KEY;
 
 public class BookAdapter extends RecyclerView.Adapter<BookHolder> {
 
     private List<Book> mBook = new ArrayList<>();
+    private UserSettings mUserSettings;
+    private Set<String> favorites = new HashSet<>();
 
     @NonNull
     @Override
@@ -41,6 +49,10 @@ public class BookAdapter extends RecyclerView.Adapter<BookHolder> {
                     book.setBookIsFavorite(true);
                     holder.mFavoriteIcon.setImageResource(R.drawable.like);
                     FavoriteBooksFragment.mBookAdapter.addFavoriteBook(book);
+                    //favorites.add("" + book.getId());
+                    MainActivity.mUserSettings.mSharedPreferences.edit().putInt(FAVORITE_BOOKS_KEY, book.getId()).apply();
+                    //MainActivity.mUserSettings.mSharedPreferences.edit().putStringSet(FAVORITE_BOOKS_KEY, favorites).apply();
+                    //Log.d(TAG, "onClick: " + favorites.toString());
                 }
                 else {
                     book.setBookIsFavorite(false);
@@ -63,13 +75,33 @@ public class BookAdapter extends RecyclerView.Adapter<BookHolder> {
     public void createBooksStub(){
         for (int i=1;i<=11;i++){
 
+            Book book;
+
             // Создание коротких описаний для каждой книги
             int lastDotIndex = booksDescription[i-1].indexOf(".", 100);
             int lastTrimIndex = booksDescription[i-1].indexOf(" ", 100);
 
-            if (lastDotIndex < 150)
-                mBook.add(new Book(i, imagesCollection[i-1], booksNames[i-1], booksDescription[i-1].substring(0, lastDotIndex).concat("...")));
-            else mBook.add(new Book(i, imagesCollection[i-1], booksNames[i-1], booksDescription[i-1].substring(0, lastTrimIndex).concat("...")));
+            if (lastDotIndex < 150){
+                if(i == MainActivity.mUserSettings.mSharedPreferences.getInt(FAVORITE_BOOKS_KEY, 1)){
+                    book = new Book(i, imagesCollection[i-1], booksNames[i-1], booksDescription[i-1].substring(0, lastDotIndex).concat("..."), true);
+                    FavoriteBooksFragment.mBookAdapter.mBook.add(book);
+                    mBook.add(book);
+                }else {
+                    book = new Book(i, imagesCollection[i-1], booksNames[i-1], booksDescription[i-1].substring(0, lastDotIndex).concat("..."), false);
+                    mBook.add(book);
+                }
+
+            }
+            else {
+                if(i == MainActivity.mUserSettings.mSharedPreferences.getInt(FAVORITE_BOOKS_KEY, 1)){
+                    book = new Book(i, imagesCollection[i-1], booksNames[i-1], booksDescription[i-1].substring(0, lastTrimIndex).concat("..."), true);
+                    FavoriteBooksFragment.mBookAdapter.mBook.add(book);
+                    mBook.add(book);
+                }else {
+                    book = new Book(i, imagesCollection[i-1], booksNames[i-1], booksDescription[i-1].substring(0, lastTrimIndex).concat("..."), false);
+                    mBook.add(book);
+                }
+            }
         }
 
         notifyDataSetChanged();
--- app/src/main/java/ru/steeshock/goosebumpsapp/ui/MainActivity.java
+++ app/src/main/java/ru/steeshock/goosebumpsapp/ui/MainActivity.java
@@ -4,13 +4,23 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import ru.steeshock.goosebumpsapp.R;
+import ru.steeshock.goosebumpsapp.utils.UserSettings;
+
+import static ru.steeshock.goosebumpsapp.utils.UserSettings.FAVORITE_BOOKS_KEY;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "KEKE";
     private PagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
+    public static UserSettings mUserSettings;
+    private Set<String> favorites = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        mUserSettings = new UserSettings(this);
+        Log.d(TAG, "onCreate: " + mUserSettings.mSharedPreferences.getInt(FAVORITE_BOOKS_KEY, -1));
+        //Log.d(TAG, "onCreate: " + mUserSettings.mSharedPreferences.getStringSet(FAVORITE_BOOKS_KEY, favorites));
+
     }
 }
