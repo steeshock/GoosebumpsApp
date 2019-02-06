aa2efe8e77c4d4cdddebdded439ebed2e5786b29
Eugeny <steeshock@gmail.com>
2019-02-06 19:20:20 +0300

    Simple NightMode + one adopted book (im so tired do to it..)

--- app/src/main/java/ru/steeshock/goosebumpsapp/ui/ReaderActivity.java
+++ app/src/main/java/ru/steeshock/goosebumpsapp/ui/ReaderActivity.java
@@ -1,9 +1,12 @@
 package ru.steeshock.goosebumpsapp.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
@@ -26,10 +29,13 @@ public class ReaderActivity extends AppCompatActivity implements OnPageChangeLis
 
     private static final String TAG = "KEKE";
     private PDFView mPDFView;
-    private Integer mPageNumber = 0;
+    private Integer mPageNumber = 1;
+    private ImageView mImageView;
+    private LinearLayout mMainContent;
+
     private int mId;
-    private Button mButton;
-    private int counter = 0;
+    private static boolean nightMode = true;
+    private static boolean isReaderClicked = true;
 
 
     @Override
@@ -41,30 +47,98 @@ public class ReaderActivity extends AppCompatActivity implements OnPageChangeLis
             mPageNumber = savedInstanceState.getInt(SAVED_PAGE);
 
         mPDFView = findViewById(R.id.pdfView);
-        mButton = findViewById(R.id.chBtn);
+        mImageView = findViewById(R.id.ivNightMode);
+        mMainContent = findViewById(R.id.main_content);
 
         mId = getIntent().getIntExtra(BOOK_ID, 0);
-        readBookFromAsset(booksPaths[mId]);
+        readBookFromAsset(booksPaths[mId-1]);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+        setDayOrNightModeForIcon();
+        hideSystemUI();
+
+        mPDFView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changePath();
+                hideSystemUI();
+                hideNightModeIcon();
             }
         });
 
-
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMode();
+            }
+        });
     }
 
-    private void changePath(){
+    private void hideNightModeIcon() {
+        if (isReaderClicked){
+            isReaderClicked = false;
+            mImageView.setVisibility(View.INVISIBLE);
+        } else {
+            isReaderClicked = true;
+            mImageView.setVisibility(View.VISIBLE);
+        }
+    }
 
-        if (counter >= booksPaths.length)
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    private void changeMode(){
+
+        /*if (counter >= booksPaths.length)
             counter = 0;
 
         String newPath = booksPaths[counter];
         counter++;
-        readBookFromAsset(newPath);
+        readBookFromAsset(newPath);*/
 
+        nightMode = !nightMode;
+        setDayOrNightModeForIcon();
+        setDayOrNightBackgroundForMainContent();
+        mPDFView.setNightMode(nightMode);
+        mPDFView.loadPages();
+
+    }
+
+    private void setDayOrNightBackgroundForMainContent() {
+        if(nightMode){
+            mMainContent.setBackgroundColor(Color.BLACK);
+            mPDFView.setBackgroundColor(Color.BLACK);
+        }else {
+            mMainContent.setBackgroundColor(Color.WHITE);
+            mPDFView.setBackgroundColor(Color.WHITE);
+        }
+
+    }
+
+    private void setDayOrNightModeForIcon() {
+        if(nightMode)
+            mImageView.setImageResource(R.drawable.night_on);
+        else mImageView.setImageResource(R.drawable.night_off);
     }
 
     private void readBookFromAsset(String path){
@@ -75,9 +149,14 @@ public class ReaderActivity extends AppCompatActivity implements OnPageChangeLis
                 .enableAnnotationRendering(true)
                 .onLoad(this)
                 .scrollHandle(new DefaultScrollHandle(this))
-                .spacing(10) // in dp
+                //.spacing(10) // in dp
                 .onPageError(this)
-                .pageFitPolicy(FitPolicy.BOTH)
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .swipeHorizontal(true)
+                .pageSnap(true)
+                .autoSpacing(true)
+                .pageFling(true)
+                .nightMode(nightMode)
                 .load();
 
     }
